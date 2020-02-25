968783d0d2ee33e59c3f6b8b57a4d4d97bab73b8
anandbabu2201 <anandbabu2201@gmail.com>
2020-02-26 01:21:34 +0530

    third commit

--- src/actions/dataActions.js
+++ src/actions/dataActions.js
@@ -1,10 +1,16 @@
-import { FETCHING_DATASUCESS } from "./types";
+import { FETCHING_DATASUCESS, FETCHING_DATA, FETCHING_FAILURE } from "./types";
 import  axios from 'axios';
 
 export const getData =_=> async dispatch =>{
-    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
-    dispatch({
-        type: FETCHING_DATASUCESS,
-        payload:res.data
-    })
+    dispatch({type:FETCHING_DATA});
+    try {
+        const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+        dispatch({
+            type: FETCHING_DATASUCESS,
+            payload:res.data
+        })
+
+    } catch(err){
+        dispatch({type:FETCHING_FAILURE})
+    }
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -5,11 +5,14 @@ import { getData }  from '../actions/dataActions'
 class Button extends Component {
 
     render(){
+        const { data=[] , loading} = this.props.data || {}
+        const items = data.map(e=> <div key={e.id}>{e.name}</div>);
         return (
             <React.Fragment>
             <button onClick={this.props.getData}> Get Data </button>
             <div>
-
+             {loading?'i am loading':''}
+             {items}
             </div>
             </React.Fragment>
         )
